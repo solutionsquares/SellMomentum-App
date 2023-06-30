2c275483ed4a3d505f1dfd57c218c67ba07cfe60
solutionsquares <usman@solutionsquares.com>
2023-06-30 18:31:13 +0530

    add products working

--- src/screens/Product/addProduct.js
+++ src/screens/Product/addProduct.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   SafeAreaView,
   View,
@@ -7,49 +7,73 @@ import {
   TouchableOpacity,
   StyleSheet,
   TextInput,
-  Button
+  Image
+
 } from 'react-native'
+import { SelectList } from 'react-native-dropdown-select-list'
+import {launchImageLibrary} from 'react-native-image-picker';
+
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchUser, selectAll } from '../../stores/user.reducer'
-import BackButton from '../../components/backButton'
 import Input from '../../components/textInput'
-import Header from '../../components/header'
-import { ALERT_TYPE, Dialog, AlertNotificationRoot, Toast } from 'react-native-alert-notification';
-import { DialogMsgClose, DialogMsg, ToastMsg, ToastMsgClose } from '../../utils/notification'
-import { constant } from '../../constant/constant'
+// import ImagePicker from 'react-native-image-picker';
+import { ScrollView } from 'react-native-gesture-handler';
+import { addProducts } from '../../api/productApi'
+// var ImagePicker = require('react-native-image-picker');
+
 const theme = require('../../core/theme');
 const AddProduct = ({ navigation }) => {
-  const [email, setEmail] = useState({ value: '', error: '' })
-  const [password, setPassword] = useState({ value: '', error: '' })
-  const [ApiLoader, setApiLoader] = useState(false);
+  const [productName, setProductName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [inStock, setInStock] = useState('');
+  const [categoryId, setCategoryId] = useState('');
+  const [imageUri, setImageUri] = useState("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60");
+  const [error, setError] = useState('');
   const dispatch = useDispatch()
-  const onLoginPressed = ({ navigation }) => {
-    const emailError = emailValidator(email.value)
-    const passwordError = passwordValidator(password.value)
-   
-    if (emailError || passwordError) {
-      setEmail({ ...email, error: emailError })
-      setPassword({ ...password, error: passwordError })
-      return
+  let isReadGranted;
+  const reqPermission = async () => {
+    if (Platform.OS === 'android') {
+      isReadGranted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      );
+      console.log('isReadGranted', isReadGranted);
     }
-    
-  }
+  };
 
-  async function LoginFun() {
-    console.log(email)
-    console.log(password)
-    setApiLoader(true)
-    await dispatch(fetchUser({
-      "email": email.value,
-      "password": password.value
-    })).then(async (res) => {
+
+  const category = useSelector(state => state?.category?.entities?.undefined)
+
+
+  const categories = category;
+  const categoriesList = categories.map(({ name, sellerId }) => ({
+    value: name,
+    key: sellerId,
+  }));
+
+  useEffect(()=>{
+   reqPermission() 
+  })
+
+  async function handleAddProduct() {
+    validate()
+    console.log(productName)
+    console.log(categoryId);
+    const formData = new FormData();
+    formData.append('productName', productName);
+    formData.append('description', description);
+    formData.append('price', price);
+    formData.append('inStock', inStock);
+    formData.append('categoryId', categoryId);
+    formData.append('productImage', imageUri)    // setApiLoader(true)
+    console.log(formData)
+    addProducts(formData).then(async (res) => {
       if (res) {        
         console.log("res",res)
         try {
         } catch (error) {
           console.log("error",error)
         }
-        
+
         setApiLoader(false)
         ToastMsg(constant.errorActionTypes.success, 'Success', 'OTP successfully send')
       } else {
@@ -57,47 +81,93 @@ const AddProduct = ({ navigation }) => {
       }
 
     })
-    
-    // setTimeout(() => {
-    //   DialogMsgClose()
-    //   navigation.replace('HomeBase', { screen: 'Home' })
-    // }, 3000)
+
 
   }
-  function SignUpPage() {
-    navigation.navigate('WithoutAuth', { screen: 'Signup' })
-  }
+  const chooseFile = type => {
+    // if (isReadGranted === 'denied') {
+    //   reqPermission();
+    // } else {
+    //   let options = {
+    //     mediaType: type,
+    //     maxWidth: 300,
+    //     maxHeight: 550,
+    //     quality: 1,
+    //   };
+    //   ImagePicker.launchImageLibrary(options, response => {
+    //         if (response.didCancel) {
+    //           console.log('User cancelled image picker');
+    //         } else if (response.error) {
+    //           console.log('Image picker error:', response.error);
+    //         } else {
+    //           const uri = response.uri;
+    //           setImageUri(uri);
+    //         }
+    //       });
+      
+    //   // launchImageLibrary(options, response => {
+    //   //   //console.log('Response = ', response);
+    //   //   if (response.assets) {
+    //   //     setFilePath(response.assets[0]);
+    //   //   } else if (response.didCancel) {
+    //   //     setError('User cancelled image picker');
+    //   //     setToast(true);
+    //   //     return;
+    //   //   } else if (response.errorCode === 'camera_unavailable') {
+    //   //     setError('Camera not available on device');
+    //   //     setToast(true);
+    //   //     return;
+    //   //   } else if (response.errorCode === 'permission') {
+    //   //     setError('Permission not satisfied');
+    //   //     setToast(true);
+    //   //     return;
+    //   //   } else if (response.errorCode === 'others') {
+    //   //     setError(response.errorMessage);
+    //   //     setToast(true);
+    //   //     return;
+    //   //   }
+    //   //   // setFilePath(response);
+    //   // });
+    // }
+  };
+
+  // const openImagePicker = () => {
+  // console.log("Temperrory disbled")
+  //   // const options = {
+  //   //   mediaType: 'photo',
+  //   //   includeBase64: false,
+  //   //   maxHeight: 200,
+  //   //   maxWidth: 200,
+  //   // };
+
+
+  //   // ImagePicker.launchImageLibrary(options, response => {
+  //   //   if (response.didCancel) {
+  //   //     console.log('User cancelled image picker');
+  //   //   } else if (response.error) {
+  //   //     console.log('Image picker error:', response.error);
+  //   //   } else {
+  //   //     const uri = response.uri;
+  //   //     setImageUri(uri);
+  //   //   }
+  //   // });
+  // };
+
+
 
   return (
     <>
 
       <View style={styles.container}>
         <StatusBar barStyle="light-content" style={[theme.primaryBGColor]} />
-        
+      <ScrollView>
         <View>
-          <Input
-            label="Email/Mobile Number"
-            returnKeyType="next"
-            value={email.value}
-            onChangeText={(text) => setEmail({ value: text, error: '' })}
-            error={!!email.error}
-            errorText={email.error}
-            autoCapitalize="none"
-            autoCompleteType="email"
-            textContentType="email"
-            keyboardType={'email'}
-            maxLength={100}
-            placeholderTextColor={theme.secondaryColor}
-            selectionColor={theme.secondaryColor}
-            style={theme.inputDark}
-          />
           <Input
             label="Product Name"
             returnKeyType="next"
-            value={email.value}
-            onChangeText={(text) => setEmail({ value: text, error: '' })}
-            error={!!email.error}
-            errorText={email.error}
+            value={productName}
+            onChangeText={(text) => setProductName(text)}
+            errorText={error}
             autoCapitalize="none"
             autoCompleteType="text"
             textContentType="text"
@@ -105,99 +175,85 @@ const AddProduct = ({ navigation }) => {
             maxLength={100}
             placeholderTextColor={theme.secondaryColor}
             selectionColor={theme.secondaryColor}
-            style={theme.inputDark}
+            style={theme.inputDarkSquare}
           />
-          <Input
-            label="Description"
-            returnKeyType="next"
-            value={email.value}
-            onChangeText={(text) => setEmail({ value: text, error: '' })}
-            error={!!email.error}
-            errorText={email.error}
-            autoCapitalize="none"
-            autoCompleteType="text"
-            textContentType="text"
-            keyboardType={'text'}
-            maxLength={100}
-            placeholderTextColor={theme.secondaryColor}
-            selectionColor={theme.secondaryColor}
-            style={theme.inputDark}
-          />
-          <Input
-            label="Price"
-            returnKeyType="next"
-            value={email.value}
-            onChangeText={(text) => setEmail({ value: text, error: '' })}
-            error={!!email.error}
-            errorText={email.error}
-            autoCapitalize="none"
-            autoCompleteType="text"
-            textContentType="text"
-            keyboardType={'text'}
-            maxLength={100}
-            placeholderTextColor={theme.secondaryColor}
-            selectionColor={theme.secondaryColor}
-            style={theme.inputDark}
-          />
-          <Input
-            label="In Stock"
-            returnKeyType="next"
-            value={email.value}
-            onChangeText={(text) => setEmail({ value: text, error: '' })}
-            error={!!email.error}
-            errorText={email.error}
-            autoCapitalize="none"
-            autoCompleteType="text"
-            textContentType="text"
-            keyboardType={'text'}
-            maxLength={100}
-            placeholderTextColor={theme.secondaryColor}
-            selectionColor={theme.secondaryColor}
-            style={theme.inputDark}
-          />
-          <Input
-            label="In Category"
-            returnKeyType="next"
-            value={email.value}
-            onChangeText={(text) => setEmail({ value: text, error: '' })}
-            error={!!email.error}
-            errorText={email.error}
-            autoCapitalize="none"
-            autoCompleteType="text"
-            textContentType="text"
-            keyboardType={'text'}
-            maxLength={100}
-            placeholderTextColor={theme.secondaryColor}
-            selectionColor={theme.secondaryColor}
-            style={theme.inputDark}
-          />
-          
 
-          {/* <TextInput
-            style={theme.input}
-            placeholder="Password"
-            placeholderTextColor={theme.colors.white}
-            secureTextEntry
-          /> */}
-          <TouchableOpacity onPress={() => LoginFun()}>
-            <View style={[styles.nextButton,theme.primaryBGColor]}>
+          <Input
+            label="Product Description"
+            returnKeyType="next"
+            value={description}
+            onChangeText={(text) => setDescription(text)}
+            // error={!!email.error}
+            // errorText={email.error}
+            autoCapitalize="none"
+            autoCompleteType="text"
+            textContentType="text"
+            keyboardType={'text'}
+            maxLength={100}
+            placeholderTextColor={theme.secondaryColor}
+            selectionColor={theme.secondaryColor}
+            style={theme.inputDarkSquare}
+          />
+          <Input
+            label="Product Price"
+            returnKeyType="next"
+            value={price}
+            onChangeText={(text) => setPrice(text)}
+            // error={!!email.error}
+            // errorText={email.error}
+            autoCapitalize="none"
+            autoCompleteType="text"
+            textContentType="text"
+            keyboardType={'numeric'}
+            maxLength={100}
+            placeholderTextColor={theme.secondaryColor}
+            selectionColor={theme.secondaryColor}
+            style={theme.inputDarkSquare}
+          />
+          <Input
+            label="Product Available Stock"
+            returnKeyType="next"
+            value={inStock}
+            onChangeText={(text) => setInStock(text)}
+            // error={!!email.error}
+            // errorText={email.error}
+            autoCapitalize="none"
+            autoCompleteType="text"
+            textContentType="text"
+            keyboardType={'numeric'}
+            maxLength={100}
+            placeholderTextColor={theme.secondaryColor}
+            selectionColor={theme.secondaryColor}
+            style={theme.inputDarkSquare}
+          />
+
+          <SelectList
+            setSelected={(val, key) => setCategoryId(val)}
+            onSelect={(categoryId) => {
+              console.log(categoryId)
+
+            }}
+            data={categoriesList}
+            save="value"
+          />
+          {imageUri && (
+            <Image
+              source={{ uri: imageUri }}
+              style={{ width: 200, height: 200, marginLeft:'auto',marginRight:'auto'}}
+            />
+          )}
+          <TouchableOpacity onPress={() => chooseFile()}>
+            <View style={[styles.nextButton, theme.primaryBGColor]}>
+              <Text style={theme.whiteText}>Upload Products Images</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleAddProduct()}>
+            <View style={[styles.nextButton, theme.primaryBGColor]}>
               <Text style={theme.whiteText}>Submit</Text>
             </View>
           </TouchableOpacity>
-          {/* <View style={theme.centerCss}>
-            <Text style={[theme.whiteText, styles.allMargen]}>Forgot your password?</Text>
-          </View>
-          <View style={[theme.centerCss, theme.rowView]}>
-            <View>
-              <Text style={[theme.whiteText, styles.allMargen]}>Don’t have an account? </Text>
-            </View>
-            <TouchableOpacity onPress={() => SignUpPage()}>
-              <View>
-                <Text style={[theme.whiteText, styles.allMargen, styles.boldText]}>Sign up</Text>
-              </View>
-            </TouchableOpacity>
-          </View> */}
         </View>
+        </ScrollView>
 
       </View>
     </>
@@ -223,8 +279,8 @@ const styles = StyleSheet.create({
   allMargen: {
     margin: 20
   },
-  
- 
+
+
 
 })
 
--- src/stores/user.reducer.js
+++ src/stores/user.reducer.js
@@ -3,7 +3,7 @@ import {
   createAsyncThunk,
   createEntityAdapter
 } from '@reduxjs/toolkit'
-import { getUser,registerUser } from '../api/ApiUser'
+import { getUser,registerUser,logOutUser } from '../api/ApiUser'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { constant } from '../constant/constant';
 
@@ -19,6 +19,12 @@ export const registerUsers = createAsyncThunk('user/registerUser', async (obj) =
   console.log(response)
   return response
 })
+export const LogoutUser = createAsyncThunk('user/getUser', async (obj) => {
+  console.log(obj)
+  const response = await logOutUser(obj)
+  console.log(response)
+  return response
+})
 
 
 
@@ -30,24 +36,30 @@ const userSlice = createSlice({
   reducers: {},
   extraReducers: builder => {
     builder
-      
       .addCase(fetchUser.fulfilled, (state, action) => {
-        console.log(state)
-        console.log(action)
         if(action.payload) {
-          userAdapter.setAll(state, [action.meta.arg])
+          userAdapter.setAll(state, [action.payload.data])
         }
         userAdapter.getSelectors(state => state.user)
       })
-    
-      // .addCase(selectAll.fulfilled, (state, action) => {
-      //   console.log("sgr",action.payload)
-      //   if(action.payload) userAdapter.setAll(state, action.payload)
-      //   userAdapter.getSelectors(state => state.members)
-      //   state.isLoading = false
-
-      // })
+   
   }
+
+
+  // extraReducers: builder => {
+  //   builder
+  //     .addCase(fetchUser.pending, (state, action) => {
+  //       state.isLoading = true
+  //     })
+  //     .addCase(fetchUser.fulfilled, (state, action) => {
+  //       console.log("fetchUser setAll")
+  //       userAdapter.setAll(state, action.payload)
+  //       state.isLoading = false
+  //     })
+  //     .addCase(fetchUser.rejected, state => {
+  //       state.isLoading = false
+  //     })
+  // }
 })
 
 
